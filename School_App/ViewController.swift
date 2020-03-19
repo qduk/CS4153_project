a052d8dd0d872e63597fd02675b92ee8de89ae07
Adam Byczkowski <abyczko@ostatemail.okstate.edu>
2020-03-19 01:07:42 -0500

    Initial Commit

---
+++ School_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  School_App
+//
+//  Created by Adam B on 3/19/20.
+//  Copyright © 2020 Adam B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

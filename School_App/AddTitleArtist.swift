1b92a8d0b42eec99b1b8ed10ca1b1857d8a9c68c
Adam Byczkowski <abyczko@ostatemail.okstate.edu>
2020-04-19 19:35:37 -0500

    Changed layout and added bar button

---
+++ School_App/AddTitleArtist.swift
@@ -0,0 +1,30 @@
+//
+//  AddTitleArtist.swift
+//  School_App
+//
+//  Created by Adam B on 4/19/20.
+//  Copyright © 2020 Adam B. All rights reserved.
+//
+
+import UIKit
+
+class AddTitleArtist: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
